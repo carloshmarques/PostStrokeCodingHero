4190d45df3f90adda92d578c849348d408c87ee2
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2025-09-24 21:02:12 +0100

    all for now

---
+++ Week3_Conditions/App/App/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Program
+    {
+        static void IfEslse()
+        {
+            int age = 51;
+
+            if (age < 18)
+            {
+                Console.WriteLine("You are a minor.");
+            }
+            else
+            {
+                Console.WriteLine("You are an adult.");
+            }
+
+            Console.ReadKey();
+
+        }
+        static void NestedIf()
+        {
+            int score = 85;
+
+            if (score >= 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else
+            {
+                Console.WriteLine("Grade: C or below");
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static void SwitchCase()
+        {
+            string day = "";
+
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("Start of the week.");
+                    break;
+                case "Friday":
+                    Console.WriteLine("Almost weekend!");
+                    break;
+                default:
+                    Console.WriteLine("Just another day.");
+                    break;
+            }
+        }
+        static void runProgram()
+        {
+            IfEslse();
+            NestedIf(); 
+            SwitchCase();   
+
+        }
+
+        static void Main(string[] args)
+        {
+             runProgram();
+        }
+    }
+}
---
+++ Week4_Functions/README.md
@@ -0,0 +1,18 @@
+# 🧮 Week5_Arrays
+
+This folder contains exercises to practice working with arrays in C#.
+
+---
+
+## 🧪 Exercises
+
+- `SimpleArray.cs`: Create and print a list of fruits.
+- `ArrayIndex.cs`: Access specific elements using index.
+- `ArraySum.cs`: Calculate the total sum of values.
+
+---
+
+## 🚀 Usage
+
+These files were created using Git Bash and edited with Nano.  
+Run each `.cs` file to see how arrays help organize and process data.
